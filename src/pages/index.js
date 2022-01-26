6fd72db6a8d2c5c066b3e501f92c59c4ead66ebf
Ruoxi Shang <32284368+RuoxiShang@users.noreply.github.com>
2022-01-25 16:40:31 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Section from '../components/Section';
 import Paragraph from '../components/shared/Paragraph';
 
+/*
 export default function Index() {
   return (
     <Section title="About Me" keywords={['react', 'javascript', 'web']}>
@@ -17,6 +18,25 @@ export default function Index() {
         <abbr title="Backends for Frontends">BFF</abbr> pattern). The{' '}
         <abbr title="Keep it Simple, Stupid">KISS</abbr> principle is my mojo. I
         also try my best to act as an accessibility advocate.
+      </Paragraph>
+    </Section>
+  );
+}
+*/
+
+export default function Index() {
+  return (
+    <Section title="About Me" keywords={['react', 'javascript', 'web']}>
+      <Paragraph>
+        Hi, I’m Anna. 
+        I am a Human-Computer Interaction (HCI) researcher and a 2nd year PhD student in 
+        Human Centered Design & Engineering Department at University of Washington, 
+        advised by Dr. Gary Hsieh and Dr. Chirag Shah. 
+        My reserach interest is in Human-centered Explainable AI, 
+        and my most recent work is about understanding end-users' needs in the explainability 
+        of intelligent decision-support systems. 
+        Before coming into HCDE, I studied Applied Mathematics and Statistics. I had fun with a few Applied Data Science 
+        research projects around building Machine Learning and Deep Neural Network predictive models.
       </Paragraph>
     </Section>
   );
