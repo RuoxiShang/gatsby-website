4414ad3c0ec5e0a7d36e8671a9ca3dd11bc9166d
Ruoxi Shang <32284368+RuoxiShang@users.noreply.github.com>
2022-01-28 00:47:47 -0800

    s

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,30 +25,11 @@ export default function Footer() {
       <nav aria-label="External links">
         <List>
           <li>
-            View my complete resume on{' '}
-            <Anchor href="https://www.linkedin.com/in/sylvainhamann">
-              LinkedIn
+            View my complete resume{' '}
+            <Anchor href="../../cv-ruoxi-sp22.pdf">
+              here
             </Anchor>
             .
-          </li>
-          <li>
-            You can also download it as PDF on{' '}
-            <Anchor href="https://docs.google.com/document/d/1VybgVJOKbZOqbQwRfGkYp--9yP-xXfMF5TA7s8M250I/edit?usp=sharing">
-              Google Drive
-            </Anchor>
-            .
-          </li>
-          <li>
-            Browse my portfolio on{' '}
-            <Anchor href="https://www.behance.net/sylvhama/">Behance</Anchor>.
-          </li>
-          <li>
-            Follow me on{' '}
-            <Anchor href="https://twitter.com/sylvhama/">Twitter</Anchor>.
-          </li>
-          <li>
-            See some of my code on{' '}
-            <Anchor href="https://github.com/sylvhama">GitHub</Anchor>.
           </li>
         </List>
       </nav>
